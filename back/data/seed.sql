05df29d90b53fcf531f9c0879a43b9997e245c36
romadesign <roman247.rg@gmail.com>
2023-10-22 04:06:51 +0200

    new database

--- back/data/seed.sql
+++ back/data/seed.sql
@@ -1,9 +1,9 @@
 BEGIN TRANSACTION;
 
 INSERT INTO users VALUES 
- ('user1','user1@example.com','User 1','{hash_password[user1]}',1,'223366895','user1@example.com','bilbao'),
- ('user2','user2@example.com','User 2','{hash_password[user2]}',0,'55689854','user2@example.com','casco viejo'),
- ('user3','user3@example.com','User 3','{hash_password[user3]}',0,'55568842','user3@example.com','barakaldo'),
- ('user4','user4@example.com','User 4','{hash_password[user4]}',0,'2662266558',,'user4@example.com','muskiz');
+ ('user-1','user-1@example.com','User 1','{hash_password[newtest1]}',1,'223366895','user-1@example.com','bilbao'),
+ ('user-2','user-2@example.com','User 2','{hash_password[newtest1]}',0,'55689854','user-2@example.com','casco viejo'),
+ ('user-3','user-3@example.com','User 3','{hash_password[newtest1]}',0,'55568842','user-3@example.com','barakaldo'),
+ ('user-4','user-4@example.com','User 4','{hash_password[newtest1]}',0,'2662266558',,'user-4@example.com','muskiz');
 
 COMMIT;
